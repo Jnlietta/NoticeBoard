1a2625d21229e1767e5acbd1e3b601117d309094
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-14 14:52:02 +0200

    Add files controllers/ads.controller and controllers/auth.controller

---
+++ controllers/ads.controller.js
@@ -0,0 +1,6 @@
+exports.getAll = async (req, res) => {};
+exports.getById = async (req, res) => {};
+exports.getBySearchPhrase = async (req, res) => {};
+exports.addNew = async (req, res) => {};
+exports.editById = async (req, res) => {};
+exports.deleteById = async (req, res) => {};
