57f7deeca699041cb80f609d7ca4036ed397eb53
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-14 12:33:35 +0200

    Add proces.env.PATH in server in server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 
+//start express server
 const app = express();
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running...');
+});
 
 // add middleware 
 app.use(cors());
-//app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-app.listen(8000, () => {
-console.log('Server is running on port: 8000');
-});
+// serve static files from react app
+//app.use(express.static(path.join(__dirname, '/client/build')));
