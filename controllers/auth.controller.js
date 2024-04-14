304eab08e9753ead8cc58a51f413817ca4696548
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-14 16:02:09 +0200

    Add import of User.model and cases to verify user and res with status 409 in func register in auth.controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,20 @@
-exports.register = async (req, res) => {};
+const User = require('../models/User.model');
+
+exports.register = async (req, res) => {
+    const { login, password, avatar, phone } = req.body;
+
+    if(login && typeof login === 'string' && password && typeof password == 'string' &&
+       avatar && typeof avatar === 'string' && phone && typeof phone == 'number') {
+
+        const userWithLogin = await User.findOne({ login });
+
+        if(userWithLogin){
+            res.status(409).send({ message: 'User with this login already exists'});
+        }
+
+    }
+    res.send('register');
+};
+
 exports.login = async (req, res) => {};
 exports.getUser = async (req, res) => {};
