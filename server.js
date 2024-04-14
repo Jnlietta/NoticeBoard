89ad9973963a917916247b2d13b5b45185219c60
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-14 11:38:38 +0200

    Add middleware cors, express.json, express.urlencoded and for future commented express.static in server.js

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const cors = require('cors');
 
 const app = express();
 
+// add middleware 
+app.use(cors());
+//app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
 app.listen(8000, () => {
 console.log('Server is running on port: 8000');
 });
