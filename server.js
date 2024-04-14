19e13e01a440c4216495c5dba20a8222e4990ce0
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-14 17:55:46 +0200

    Add import of express-session and middleware session in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 //start express server
 const app = express();
@@ -13,6 +14,7 @@ const server = app.listen(process.env.PORT || 8000, () => {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(session({ secret: 'xyz567' }));
 
 // serve static files from react app
 //app.use(express.static(path.join(__dirname, '/client/build')));
