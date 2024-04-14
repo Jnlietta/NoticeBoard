a134faff7cf40ce0ddbfed357ef8378ed0c850a0
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-14 17:43:24 +0200

    Add code to endpoint login in auth.controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,8 +2,7 @@ const User = require('../models/User.model');
 const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
-
-    try{
+    try {
         const { login, password, avatar, phone } = req.body;
 
         if(login && typeof login === 'string' && password && typeof password == 'string' &&
@@ -27,5 +26,33 @@ exports.register = async (req, res) => {
     }
 };
 
-exports.login = async (req, res) => {};
+exports.login = async (req, res) => {
+    try {
+        const { login, password } = req.body;
+
+        if(login && typeof login === 'string' && password && typeof password == 'string'){
+
+            const user  = await User.findOne({ login });
+
+            if(!user){
+                res.status(400).send({ message: 'Login or password are incorrect' });
+            }
+            else {
+                if(bcrypt.compareSync(password, user.password)){
+                    res.status(200).send({ message: 'Login successful' });
+                }
+                else {
+                    res.status(400).send({ message: 'Login or password are incorrect' });
+                }
+            }
+
+        } else {
+            res.status(400).send({ message: 'Bad request' });
+        }
+
+    } catch(err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
 exports.getUser = async (req, res) => {};
