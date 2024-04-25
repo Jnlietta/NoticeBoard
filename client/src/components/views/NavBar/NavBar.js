6888df0ecf0cbf082d4655cea564ac7683961796
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-25 21:19:09 +0200

    Add code with links Home, Register, Sign in, Sign out in NavBar.js

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,6 +1,23 @@
-const NavBar = props => {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+
+
+const NavBar = ({ isLoggedIn }) => {
     return(
-        <h1>NavBar</h1>
+        <Navbar bg="primary" data-bs-theme="dark" expand="lg" className={clsx('mb-4','rounded', 'navbar-dark')}>
+            <Container>
+                <Navbar.Brand href="#home">NoticeBoard.app</Navbar.Brand>
+                <Nav className="justify-content-end">
+                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                    {!isLoggedIn && <Nav.Link as={NavLink} to="/user/register">Sign in</Nav.Link>}
+                    {!isLoggedIn && <Nav.Link as={NavLink} to="/user/login">Sign up</Nav.Link>}
+                    {isLoggedIn && <Nav.Link as={NavLink} to="/user/logout">Sign out</Nav.Link>}
+                </Nav>
+            </Container>
+        </Navbar>
     );
 };
 
