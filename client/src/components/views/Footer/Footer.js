7e704443869a75ca8e1f77c702a3b3555d53e091
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-25 21:22:15 +0200

    Add code in Footer.js

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,7 +1,8 @@
+import clsx from 'clsx';
+
 const Footer = props => {
     return(
-        <h1>Footer</h1>
-    );
+        <p className={clsx('text-center', 'text-muted')}>Copyright © NoticeBoard 2024</p>    );
 };
 
 export default Footer;
