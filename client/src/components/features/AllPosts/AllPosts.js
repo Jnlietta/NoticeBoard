f8c7a7b9071903eb20a30c3add574d358a571ec6
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-26 09:26:39 +0200

    Add Card and Button inside AllPosts.js

--- client/src/components/features/AllPosts/AllPosts.js
+++ client/src/components/features/AllPosts/AllPosts.js
@@ -1,10 +1,18 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-const Home = props => {
-    return(
-        <div>
-            <h2>All Posts</h2>
-        </div>
-    );
-};
+const AllPosts = () => {
+    
+  return (
+    <Card style={{ width: '18rem', marginTop: '20px' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>Location</Card.Text>
+        <Button variant="primary">Read more</Button>
+      </Card.Body>
+    </Card>
+  );
+}
 
-export default Home;
+export default AllPosts;
