614321661804c9bbb805442a06361dda3f394f13
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-26 20:31:51 +0200

    Use selector getAds in Home.js

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,12 +1,16 @@
-import AllPosts from '../../features/AllAds/AllAds';
+import { getAds } from '../../../redux/adsRedux';
+import AllAds from '../../features/AllAds/AllAds';
 import SearchForm from '../../features/SearchForm/SearchForm';
+import { useSelector } from 'react-redux';
 
 const Home = props => {
+    const ads = useSelector(getAds);
+
     return(
         <div>
             <h1>Home</h1>
             <SearchForm />
-            <AllPosts />
+            <AllAds data={ads} />
         </div>
     );
 };
