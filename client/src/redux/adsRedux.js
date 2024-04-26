01515a7e51aa6067901675bcb3738ef78a4b127c
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-26 10:00:35 +0200

    Add actions in adsRedux.js

--- client/src/redux/adsRedux.js
+++ client/src/redux/adsRedux.js
@@ -9,6 +9,19 @@ export const getRequests = ({ ads }) => ads.requests;
 const reducerName = 'ads';
 const createActionName = name => `app/${reducerName}/${name}`;
 
+const START_REQUEST = createActionName('START_REQUEST');
+const END_REQUEST = createActionName('END_REQUEST');
+const ERROR_REQUEST = createActionName('ERROR_REQUEST');
+
+const LOAD_ADS = createActionName('LOAD_ADS');
+const ADD_AD = createActionName('ADD_AD');
+
+export const startRequest = payload => ({ payload, type: START_REQUEST });
+export const endRequest = payload => ({ payload, type: END_REQUEST });
+export const errorRequest = payload => ({ payload, type: ERROR_REQUEST });
+
+export const loadAds = payload => ({ payload, type: LOAD_ADS });
+export const addAd = payload => ({ payload, type: ADD_AD });
 
 /* THUNKS */
 
