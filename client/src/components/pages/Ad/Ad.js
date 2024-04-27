fd9543306d61a94c275debb39a1dba25f97cb486
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-27 18:40:06 +0200

    Add file Ad.module and import styles in Ad.js

--- client/src/components/pages/Ad/Ad.js
+++ client/src/components/pages/Ad/Ad.js
@@ -1,3 +1,4 @@
+import styles from './Ad.module.scss';
 import { useSelector } from 'react-redux';
 import { getAd } from '../../../redux/adsRedux';
 import { useParams, NavLink } from 'react-router-dom';
@@ -10,23 +11,22 @@ const Ad = props => {
     const ad = useSelector(state => getAd(state, id));
 
     return(
-        <article>
-            <div>
+        <article className={styles.advert}>
+            <div className={styles.header}>
                 <h2>{ad.title}</h2>
-                <div>
+                <div className={styles.buttons}>
                     <Button variant="outline-info" as={NavLink} to={"/ad/edit/" + ad.id}>Edit</Button>
                     <Button variant="outline-danger">Delete</Button>
                 </div>
             </div>
-            <img src={`${IMAGES_URL}/${ad.photo}`} alt={ad.title} />
-            <div>
+            <img src={`${IMAGES_URL}/${ad.photo}`} alt={ad.title} className={styles.image} />
+            <div className={styles.text}>
                 <p className="mb-0"><span>Author: </span>{ad.seller}</p>
                 <p className="mb-0"><span>Price: </span>{ad.price} $</p>
                 <p className="mb-0"><span>Published: </span>{formatDate(ad.date)}</p>
                 <p className="mb-0"><span>Location: </span>{ad.location}</p>
                 <br />
                 <p className="mb-0">{ad.content}</p>
-
             </div>
         </article>    
     );
