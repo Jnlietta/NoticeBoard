4041beec50bd77d2763b703c3c0b087afff5f64e
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-27 15:13:05 +0200

    Add local state searchString, add codes for handleSubmit, handleChange and use hook useNavigate in SearchForm.js

--- client/src/components/features/SearchForm/SearchForm.js
+++ client/src/components/features/SearchForm/SearchForm.js
@@ -1,27 +1,34 @@
 import { Form, FormControl } from 'react-bootstrap';
 import clsx from 'clsx';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SearchForm = () => {
+  const [ searchString, setSearchString ] = useState('');
+  
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-      };
+  const handleChange = e => {
+    e.preventDefault();
+    setSearchString(e.target.value);
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/ad/search/${searchString}`);
+  };
 
-    const handleChange = e => {
-        e.preventDefault();
-    };
-
-    return (
-        <Form inline onSubmit={handleSubmit} className={clsx('justify-content-center', 'align-items-center', 'd-flex', 'h-100')}>
-            <FormControl
-              type="text"
-              placeholder="Search..."
-              className={clsx('mr-sm-2', 'w-50')}
-              //value={searchTerm}
-              onChange={handleChange}
-              style={{textAlign: 'center'}}
-            />
-        </Form>
+  return (
+    <Form inline onSubmit={handleSubmit} className={clsx('justify-content-center', 'align-items-center', 'd-flex', 'h-100')}>
+      <FormControl
+        type="text"
+        placeholder="Search..."
+        className={clsx('mr-sm-2', 'w-50')}
+        value={searchString}
+        onChange={handleChange}
+        style={{textAlign: 'center'}}
+        />
+      </Form>
     );
   };
 
