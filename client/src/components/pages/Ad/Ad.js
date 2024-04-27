99fba29437e822824e2afbb1f5db90b2c184e2ef
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-27 18:43:47 +0200

    Add case if isLoggedIn in Ad.js

--- client/src/components/pages/Ad/Ad.js
+++ client/src/components/pages/Ad/Ad.js
@@ -6,7 +6,7 @@ import { Button } from 'react-bootstrap';
 import { IMAGES_URL } from '../../../config';
 import formatDate from '../../../utils/formatDate';
 
-const Ad = props => {
+const Ad = ({ isLoggedIn }) => {
     const {id} = useParams();
     const ad = useSelector(state => getAd(state, id));
 
@@ -14,10 +14,12 @@ const Ad = props => {
         <article className={styles.advert}>
             <div className={styles.header}>
                 <h2>{ad.title}</h2>
+                {isLoggedIn && 
                 <div className={styles.buttons}>
                     <Button variant="outline-info" as={NavLink} to={"/ad/edit/" + ad.id}>Edit</Button>
                     <Button variant="outline-danger">Delete</Button>
                 </div>
+                }
             </div>
             <img src={`${IMAGES_URL}/${ad.photo}`} alt={ad.title} className={styles.image} />
             <div className={styles.text}>
