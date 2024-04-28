6380260719d2e3dda8c2fa1c80cc9b83d5806464
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-28 20:16:49 +0200

    Fix name of selector ggetAd in EditAdForm

--- client/src/components/features/EditAdForm/EditAdForm.js
+++ client/src/components/features/EditAdForm/EditAdForm.js
@@ -1,12 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
-import { editAd, getAdById } from "../../../redux/adsRedux";
+import { editAd, getAd } from "../../../redux/adsRedux";
 import { Navigate, useNavigate } from "react-router-dom";
 import AdForm from "../AdForm/AdForm";
 
 const EditAdForm = () => {
 
     const id = window.location.pathname.split('/').filter(Boolean).pop();
-    const adToEdit = useSelector(state => getAdById(state, id));
+    const adToEdit = useSelector(state => getAd(state, id));
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
