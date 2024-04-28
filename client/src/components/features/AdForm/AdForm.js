20051af5058b806883a2e1a897ff9d6063ab3893
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-28 20:26:46 +0200

    Fix date format in AdForm

--- client/src/components/features/AdForm/AdForm.js
+++ client/src/components/features/AdForm/AdForm.js
@@ -12,7 +12,7 @@ const AdForm = ({ action, actionText, ...props }) => {
     const [seller, setSeller] = useState(props.seller || '');
     const [photo, setPhoto] = useState(props.photo || '');
     const [price, setPrice] = useState(props.price || '');
-    const [date, setDate] = useState(props.date || '');
+    const [date, setDate] = useState(new Date(props.date) || new Date());
     const [location, setLocation] = useState(props.location || '');
     const [content, setContent] = useState(props.content || '');
 
