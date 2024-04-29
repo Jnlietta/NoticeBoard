78c521edb851fbb7a485688b07b70cb1617716ab
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-29 22:26:49 +0200

    Add useEffect and action logOut inside handleSubmit in Logout.js

--- client/src/components/pages/Logout/Logout.js
+++ client/src/components/pages/Logout/Logout.js
@@ -1,14 +1,22 @@
+import { useEffect } from "react";
 import { API_URL } from "../../../config";
+import { logOut } from "../../../redux/authRedux";
+import { useDispatch } from "react-redux";
 
 const Logout = () => {
-    const options = {
-        method: 'DELETE',
-    };
+    const dispatch = useDispatch();
 
-    fetch(`${API_URL}/auth/logout`, options)
-        .then(res => {
+    useEffect(() => {
+        const options = {
+            method: 'DELETE',
+        };
+    
+        fetch(`${API_URL}/auth/logout`, options)
+            .then(res => {
+                dispatch(logOut());
+            });
+    }, [dispatch]);
 
-        });
 
     return null;
 }
