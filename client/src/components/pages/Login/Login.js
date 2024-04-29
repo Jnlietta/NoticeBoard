523d54bb57d1273c2ef9b165a32c5c45931b1041
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-29 21:38:39 +0200

    Add Form for login, password and Button to submit in Login.js

--- client/src/components/pages/Login/Login.js
+++ client/src/components/pages/Login/Login.js
@@ -1,6 +1,37 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+
 const Login = props => {
+
+    const [ login, setLogin ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const [ status, setStatus ] = useState(null); // null, loading, success, serverError, clientError, loginError
+
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+
     return(
-        <h1>Login</h1>
+        <Form className=" col-12 col-sm-3 mx-auto" onSubmit={handleSubmit}>
+
+            <h1 className="my-4">Login</h1>
+
+            <Form.Group className="mb-3" controlId="formLogin">
+                <Form.Label>Login</Form.Label>
+                <Form.Control type="text" value={login} onChange={e => setLogin(e.target.value)} placeholdder="Enter login" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholdder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">Sign up</Button>
+
+        </Form>
     );
 };
 
