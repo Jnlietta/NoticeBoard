f91ebe673a7d6bee11f1d74223b19970c76a87b8
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-29 21:46:09 +0200

    Add Alerts, Spinner components and fetch inside handleSubmit in Login.js

--- client/src/components/pages/Login/Login.js
+++ client/src/components/pages/Login/Login.js
@@ -1,23 +1,72 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from "react-bootstrap/Form";
+import { API_URL } from '../../../config';
+import { Alert, Spinner } from 'react-bootstrap';
 
 const Login = props => {
 
     const [ login, setLogin ] = useState('');
     const [ password, setPassword ] = useState('');
 
-    const [ status, setStatus ] = useState(null); // null, loading, success, serverError, clientError, loginError
+    const [ status, setStatus ] = useState(null); // null, loading, success, serverError, clientError
 
 
     const handleSubmit = e => {
         e.preventDefault();
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ login, password })
+        };
+
+        setStatus('loading');
+        fetch(`${API_URL}/auth/login`, options)
+            .then(res => {
+                if (res.status === 200) {
+                    setStatus('success');
+                } else if (res.status === 400) {
+                    setStatus('clientError');
+                } else {
+                    setStatus('serverError');
+                }
+            })
     }
 
     return(
         <Form className=" col-12 col-sm-3 mx-auto" onSubmit={handleSubmit}>
 
             <h1 className="my-4">Login</h1>
+
+            {status === "success" && (
+                <Alert variant="success">
+                    <Alert.Heading>Success!</Alert.Heading>
+                    <p>You have been successfully logged in!</p>
+                </Alert>
+            )}
+            
+            {status === "serverError" && (
+                <Alert variant="danger">
+                    <Alert.Heading>Something went wrong..</Alert.Heading>
+                    <p>Unexpected error.. Try again!</p>
+                </Alert>
+            )}
+
+            {status === "clientError" && (
+                <Alert variant="danger">
+                    <Alert.Heading>Incorrect data</Alert.Heading>
+                    <p>Login or password are incorrect.</p>
+                </Alert>
+            )}
+
+            {status === "loading" && (
+                <Spinner animation="border" role="status" className="block mx-auto">
+                    <span className="visually-hidden">Loading..</span>
+                </Spinner>
+            )}
 
             <Form.Group className="mb-3" controlId="formLogin">
                 <Form.Label>Login</Form.Label>
