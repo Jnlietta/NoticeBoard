e2d7ef18c779915f98cf50ea66f26371947636e3
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-29 19:50:25 +0200

    Add Form for login, password and Button to submit in Register.js

--- client/src/components/pages/Register/Register.js
+++ client/src/components/pages/Register/Register.js
@@ -1,6 +1,23 @@
+import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+
 const Register = props => {
     return(
-        <h1>Register</h1>
+        <Form>
+
+            <Form.Group className="mb-3" controlId="formLogin">
+                <Form.Label>Login</Form.Label>
+                <Form.Control type="text" placeholdder="Enter login" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholdder="Password" />
+            </Form.Group>
+
+            <Button variant="promary" type="submit">Submit</Button>
+            
+        </Form>
     );
 };
 
