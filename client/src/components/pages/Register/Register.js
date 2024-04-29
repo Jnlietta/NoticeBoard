adedddf71e7494d1a0f678df0950bdfb4bad3bf5
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-29 19:55:59 +0200

    Add Form for avatar, phone and add styles, header  in Register.js

--- client/src/components/pages/Register/Register.js
+++ client/src/components/pages/Register/Register.js
@@ -3,7 +3,9 @@ import Form from "react-bootstrap/Form";
 
 const Register = props => {
     return(
-        <Form>
+        <Form className=" col-12 col-sm-3 mx-auto">
+
+            <h1 className="my-4">Sign in</h1>
 
             <Form.Group className="mb-3" controlId="formLogin">
                 <Form.Label>Login</Form.Label>
@@ -15,8 +17,18 @@ const Register = props => {
                 <Form.Control type="password" placeholdder="Password" />
             </Form.Group>
 
-            <Button variant="promary" type="submit">Submit</Button>
-            
+            <Form.Group className="mb-3" controlId="formPhone">
+                <Form.Label>Phone number</Form.Label>
+                <Form.Control type="tel" placeholdder="Phone number" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formFile">
+                <Form.Label>Avatar</Form.Label>
+                <Form.Control type="file" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">Submit</Button>
+
         </Form>
     );
 };
