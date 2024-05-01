d4c259a61c6aa4a037333e8c3506e88bc3f3cf8a
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-01 12:59:26 +0200

    Add authMiddleware for post put delete in  ads.routes

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const AdsController = require('../controllers/ads.controller');
 const imageUpload = require('../utils/imageUpload');
+const authMiddleware = require('../utils/authMiddleware');
 
 router.get('/ads', AdsController.getAll);
 router.get('/ads/:id', AdsController.getById);
 router.get('/ads/search/:searchPhrase', AdsController.getBySearchPhrase);
-router.post('/ads', imageUpload.single('photo'), AdsController.addNew);
-router.put('/ads/:id', imageUpload.single('photo'), AdsController.editById);
-router.delete('/ads/:id', AdsController.deleteById);
+router.post('/ads', authMiddleware, imageUpload.single('photo'), AdsController.addNew);
+router.put('/ads/:id', authMiddleware, imageUpload.single('photo'), AdsController.editById);
+router.delete('/ads/:id', authMiddleware, AdsController.deleteById);
 
 module.exports = router;
